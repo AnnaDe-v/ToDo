d92d184a3031eb605a5a4b53f968ba7dd0d1e921
AnnaDe-v <annade.v88@gmail.com>
2022-01-10 10:25:29 +0300

    added thunks

---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Checkbox from "@mui/material/Checkbox";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import FormLabel from "@mui/material/FormLabel";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { loginTC } from "./login-reducer";
+
+type FormikErrorType = {
+  email?: string
+  password?: string
+  rememberMe?: boolean
+}
+
+export const Login = () => {
+  
+  const dispatch = useDispatch()
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+
+    validate: (values) => {
+          const errors: FormikErrorType = {};
+          if(!values.email) {
+            errors.email = 'Required';
+          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+          }
+
+          if(!values.password) {
+            errors.password = 'Пароль обязателен';
+          } else if (values.password.length < 3) {
+            errors.password = 'Короткий пароль';
+          }
+        return errors;
+    },
+    onSubmit: (values) => {
+      dispatch(loginTC(values))
+      alert(JSON.stringify(values));
+      formik.resetForm()
+    },
+  });
+
+  return (
+    <Grid container justifyContent={"center"}>
+      <Grid item justifyContent={"center"}>
+        <form
+          onSubmit={formik.handleSubmit}
+        >
+          <FormControl>
+            <FormLabel>
+              <p>
+                To log in get registered
+                <a
+                  href={"https://social-network.samuraijs.com/"}
+                  target={"_blank"}
+                >
+                  {" "}
+                  here
+                </a>
+              </p>
+              <p>or use common test account credentials:</p>
+              <p>Email: free@samuraijs.com</p>
+              <p>Password: free</p>
+            </FormLabel>
+            <FormGroup>
+              <TextField
+                label="Email"
+                margin="normal"
+                {...formik.getFieldProps("email")}
+              />
+              {formik.touched.email && formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div> : null}
+              <TextField
+                type="password"
+                label="Password"
+                margin="normal"
+                {...formik.getFieldProps("password")}
+              />
+              {formik.touched.password && formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.password}</div> : null}
+              <FormControlLabel
+                label={"rememberMe"}
+                control={
+                  <Checkbox
+                    {...formik.getFieldProps("rememberMe")}
+                    checked={formik.values.rememberMe}
+                  />
+                }
+              />
+              <Button type={"submit"} variant={"contained"} color={"primary"}>
+                Login
+              </Button>
+            </FormGroup>
+          </FormControl>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
---
+++ src/features/Login/login-reducer.ts
@@ -0,0 +1,49 @@
+import { Dispatch } from "react";
+import { authAPI, LoginParamsType } from "../../api/todolists-api";
+import {
+  SetAppErrorActionType,
+  setAppStatusAC,
+  SetAppStatusActionType,
+} from "../../app/app-reducer";
+// import { handleServerAppError } from "../../utils/error-utils"
+
+const initialState: InitialStateType = {};
+
+export const loginReducer = (
+  state: InitialStateType = initialState,
+  action: ActionsType
+): InitialStateType => {
+  switch (action.type) {
+    case "login/SET-IS-LOGGED-IN":
+      return { ...state, isLoggedIn: action.value };
+    default:
+      return state;
+  }
+};
+
+// thunks
+
+export const loginTC =
+  (data: LoginParamsType) =>
+  (dispatch: Dispatch<ActionsType | SetAppStatusActionType>) => {
+    dispatch(setAppStatusAC("loading"));
+    authAPI.login(data).then((res) => {
+      if (res.data.resultCode === 0) {
+        alert("YO");
+        dispatch(setAppStatusAC("succeeded"));
+      } else {
+        alert("NO");
+        dispatch(setAppStatusAC("succeeded"));
+      }
+    });
+  };
+
+export const setIsLoggedInAC = (value: boolean) =>
+  ({ type: "login/SET-IS-LOGGED-IN", value } as const);
+
+type ActionsType =
+  | ReturnType<typeof setIsLoggedInAC>
+  | SetAppStatusActionType
+  | SetAppErrorActionType;
+
+type InitialStateType = {};
